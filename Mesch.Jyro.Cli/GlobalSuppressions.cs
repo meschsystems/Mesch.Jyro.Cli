e0fc520bdc5555bf56b90faa2db499b05d3ce760
Mesch Systems <les.ritchin@mesch.cloud>
2025-12-07 08:41:48 +0800

    Upgrade to .Net10

--- Mesch.Jyro.Cli/GlobalSuppressions.cs
+++ Mesch.Jyro.Cli/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "Sometimes changes semantics")]
 [assembly: SuppressMessage("Performance", "CA1869:Cache and reuse 'JsonSerializerOptions' instances", Justification = "We only call it once", Scope = "member", Target = "~M:Mesch.Jyro.CliInterpreter.JyroScriptExecutor.OutputExecutionResultsAsync(Mesch.Jyro.JyroExecutionResult)~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Required for factory", Scope = "member", Target = "~M:Mesch.Jyro.Cli.JyroCommandFactory.CreateRootCommand~System.CommandLine.RootCommand")]
